a59c27e59fc633e0dab57f02e01bc3898c06152f
gamingflexer <osurve20ece@student.mes.ac.in>
2024-02-09 12:31:37 +0000

    prompt added

---
+++ src/module/prompts/base.py
@@ -0,0 +1,34 @@
+from textwrap import dedent
+
+base_prompt = dedent("""
+        ### Instruction:
+        product description starts here
+
+        {text}
+
+        product description ends here
+
+        this is the categorys list ['BEVERAGES', 'SNACKS & BRANDED FOODS', 'NOT FOUND', 'EGGS, MEAT & FISH', 'FOODGRAINS, OIL & MASALA', 'PERSONAL CARE', 'CLEANING & HOUSEHOLD', 'FRUITS & VEGETABLES', 'BAKERY, CAKES & DAIRY', 'MAKEUP', 'BABY CARE', 'PET FOOD & ACCESSORIES', 'NON FMCG', 'ALCOHOL & TOBACCO', 'WELLNESS', 'EVERYDAY MEDICINE-NEW', 'EXCERCISE & FITNESS', 'ALCOHOLIC BEVERAGES'].
+
+        Analyse data from the above product description to give me the following details in JSON format:
+        ( return "NOT FOUND" where you don't have a answer)
+        {{
+        "brand": "sample_brand",
+        "mrp": "The price might start with MRP or Rs.",
+        "unit": "per pack",
+        "parent_category": "from the above given list",
+        "ingredients": ["ingredient1", "ingredient2", "ingredient3"],
+        "calorie_count": "Would be in numbers",
+        "marketed_by": "sample_marketer",
+        "manufactured_by": "sample_manufacturer",
+        "manufactured_in_country": "Country XYZ",
+        "type_of_packaging": "Box",
+        "promotion_on_the_pack": "if any",
+        "type_of_product": "give this your understanding",
+        "pack_of_or_no_of_units": "No. of Units"
+        }}
+
+
+        Analyse data from the above product description to give me the following details in JSON format:
+        Only return the output in the required json format.
+        """)
