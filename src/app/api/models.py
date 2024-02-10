12e3df893a702aa786817a235196a9f721468941
gamingflexer <osurve20ece@student.mes.ac.in>
2024-02-10 05:48:17 +0000

    Add Product model to API

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    barcode = models.CharField(max_length=20)
+    brand = models.CharField(max_length=100)
+    sub_brand = models.CharField(max_length=100, blank=True, null=True)
+    manufacturer = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=200)
+    weight = models.FloatField()
+    variant = models.CharField(max_length=100, blank=True, null=True)
+    net_content = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    parent_category = models.CharField(max_length=100)
+    child_category = models.CharField(max_length=100)
+    sub_child_category = models.CharField(max_length=100, blank=True, null=True)
+    images_paths = models.CharField(max_length=3000, blank=True, null=True) # Comma separated paths
+
+    def __str__(self):
+        return self.product_name
