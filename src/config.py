67b7bcea560c3b663defc16ec3ecbc2a9966ed04
Vrushali-anil-Chaudhari <vchaudhari20comp@student.mes.ac.in>
2024-02-10 05:49:17 +0000

    Delete config.py and ocr.py files, update base_prompt in base.py

--- src/config.py
+++
@@ -1,7 +0,0 @@
-from decouple import config
-import os
-
-OPENAI_API_KEY = config('OPENAI_API_KEY', default="")
-emmbedding_model = "text-embedding-3-large"
-
-file_Directory= os.path.join(os.getcwd(), "data")
--- src/module/prompts/base.py
+++ src/module/prompts/base.py
@@ -12,10 +12,11 @@ base_prompt = dedent("""
 
         Analyse data from the above product description to give me the following details in JSON format:
         ( return "null" where you don't have a answer)
-        {{
+        
         "brand": "sample_brand",
         "mrp": "The price might start with MRP or Rs.",
         "unit": "per pack",
+        "Quantity": 1,  ##num of products visible
         "parent_category": "from the above given list",
         "ingredients": ["ingredient1", "ingredient2", "ingredient3"],
         "calorie_count": "Would be in numbers",
@@ -26,8 +27,7 @@ base_prompt = dedent("""
         "promotion_on_the_pack": "if any",
         "type_of_product": "give this your understanding",
         "pack_of_or_no_of_units": "No. of Units"
-        }}
-
+     
 
         Analyse data from the above product description to give me the following details in JSON format:
         Only return the output in the required json format.
