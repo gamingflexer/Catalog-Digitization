502e4cab6477641c75931fcc4d87b6f94287deab
gamingflexer <osurve20ece@student.mes.ac.in>
2024-02-10 08:30:38 +0000

    gradio app added

--- src/app.py
+++ src/app.py
@@ -1,31 +1,23 @@
-from module.config import OPENAI_API_KEY
 import gradio as gr
-import pandas as pd
 
 def sample_fun(first_image,voice_input, text_input):
     return
 
-with gr.Blocks() as demo:
+with gr.Blocks(theme=gr.themes.Default(primary_hue=gr.themes.colors.red, secondary_hue=gr.themes.colors.pink)) as demo:
     
     with gr.Tab("Add Your Image"):
+        voice_input = gr.Audio(label="Upload Audio")
+        prodcut_id = gr.Textbox(label="Enter Product ID")
         with gr.Row():
-            first_image = gr.Image(label="Upload Image")
-            voice_input = gr.Audio(label="Upload Audio")
-            text_input = gr.Textbox(label="Enter Text")
             submit_button_tab_1 = gr.Button("Start")
-        with gr.Row():
-            dataframe_output = gr.Dataframe(headers=['title','des','price'])
 
     with gr.Tab("Search Catalog"):
         with gr.Row():
-            embbed_text_search = gr.Textbox(label="Enter Text")
-        with gr.Row():
-            top_k = gr.Number(label="Number of results - Min 2")
-        with gr.Row():
+            embbed_text_search = gr.Textbox(label="Enter Product Name")
             submit_button_tab_4 = gr.Button("Start")
-            dataframe_output_tab_4 = gr.Dataframe(headers=['ID', 'Distance', 'Title', 'Authors', 'Source'])
+        dataframe_output_tab_4 = gr.Dataframe(headers=['ID', 'Distance', 'Title', 'Authors', 'Source'])
 
-    submit_button_tab_1.click(fn=sample_fun,inputs=[first_image,voice_input, text_input] ,outputs= dataframe_output)
-    submit_button_tab_4.click(fn=sample_fun,inputs=[embbed_text_search, top_k] ,outputs= dataframe_output_tab_4)
+    submit_button_tab_1.click(fn=sample_fun,inputs=[voice_input,prodcut_id])
+    submit_button_tab_4.click(fn=sample_fun,inputs=[embbed_text_search] ,outputs= dataframe_output_tab_4)
 
-demo.launch(server_name="0.0.0.0",server_port=9002)
+demo.launch(server_name="0.0.0.0",server_port=9003)
