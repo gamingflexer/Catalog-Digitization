f587433d557a51fd357d2aabe63c83a96edd699a
Vrushali-anil-Chaudhari <vchaudhari20comp@student.mes.ac.in>
2024-02-10 07:51:30 +0000

    Update .env.example and requirements.txt, delete unused files

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     barcode = models.CharField(max_length=20)
     brand = models.CharField(max_length=100)
     sub_brand = models.CharField(max_length=100, blank=True, null=True)
-    manufacturer = models.CharField(max_length=200)
+    manufactured_by = models.CharField(max_length=200)
     product_name = models.CharField(max_length=200)
     weight = models.FloatField()
     variant = models.CharField(max_length=100, blank=True, null=True)
@@ -14,6 +14,30 @@ class Product(models.Model):
     child_category = models.CharField(max_length=100)
     sub_child_category = models.CharField(max_length=100, blank=True, null=True)
     images_paths = models.CharField(max_length=3000, blank=True, null=True) # Comma separated paths
+    description = models.TextField(max_length=3000, blank=True, null=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    mrp = models.CharField(max_length=100, blank=True, null=True)
 
-    def __str__(self):
+    def _str_(self):
         return self.product_name
+
+
+class Database(models.Model):
+    barcode = models.CharField(max_length=20)
+    brand = models.CharField(max_length=100)
+    sub_brand = models.CharField(max_length=100, blank=True, null=True)
+    manufactured_by = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=200)
+    weight = models.FloatField()
+    variant = models.CharField(max_length=100, blank=True, null=True)
+    net_content = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    parent_category = models.CharField(max_length=100)
+    child_category = models.CharField(max_length=100)
+    sub_child_category = models.CharField(max_length=100, blank=True, null=True)
+    images_paths = models.CharField(max_length=3000, blank=True, null=True) # Comma separated paths
+    description = models.TextField(max_length=3000, blank=True, null=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    promotion_on_the_pack = models.CharField(max_length=100, blank=True, null=True)
+    type_of_packaging = models.CharField(max_length=100, blank=True, null=True)
+    mrp = models.CharField(max_length=100, blank=True, null=True)
